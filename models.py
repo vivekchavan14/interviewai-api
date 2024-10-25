fd3b819042eb09c7297bcebaba508a9110cb51e0
vivek <vivekchavan382@gmail.com>
2024-10-25 22:40:30 +0530

    init

---
+++ models.py
@@ -0,0 +1,19 @@
+from . import db
+
+class Interview(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    job_position = db.Column(db.String(100), nullable=False)
+    job_description = db.Column(db.Text, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    salary = db.Column(db.String(50), nullable=False)
+    experience = db.Column(db.String(50), nullable=False)
+
+    def to_dict(self):
+        return {
+            "interviewId": self.id,
+            "jobPosition": self.job_position,
+            "jobDescription": self.job_description,
+            "location": self.location,
+            "salary": self.salary,
+            "experience": self.experience,
+        }
